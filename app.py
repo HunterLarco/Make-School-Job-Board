bb26c98bc6222ae955d38f3538f568223c753646
Hunter John Larco <admin@larcolabs.com>
2015-10-11 10:56:27 -0700

    Flask Setup

---
+++ app.py
@@ -0,0 +1,40 @@
+#!flask/bin/python
+from flask import Flask
+app = Flask(__name__)
+
+from flask import send_from_directory
+from flask import request
+from flask import render_template
+
+
+
+@app.route('/jobs')
+def showjobs():
+  return render_template('content.html')
+
+
+@app.route('/')
+def login():
+  return render_template('login.html')
+
+
+@app.route('/post')
+def postjob():
+  return render_template('jobposting.html')
+
+
+@app.route('/images/<path:filename>')
+def staticimages(filename):
+    return send_from_directory(app.root_path + '/images/', filename)
+
+
+@app.route('/resources/<path:filename>')
+def staticresources(filename):
+    return send_from_directory(app.root_path + '/resources/', filename)
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
